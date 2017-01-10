e801b56b87d796809b1ec09e759518a37c7b423a
Kevin Gomez <kg-diseno@outlook.es>
2017-01-10 11:09:26 -0600

    Adding functional activity
    Creating activity that can be displayed on devices

---
+++ app/src/main/java/com/ctb/gmt/naibeck/guiamultiturismocentroamerica/ui/activity/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.ctb.gmt.naibeck.guiamultiturismocentroamerica.ui.activity;
+
+import com.ctb.gmt.naibeck.guiamultiturismocentroamerica.R;
+import com.ctb.gmt.naibeck.guiamultiturismocentroamerica.databinding.ActivityHomeBinding;
+import com.ctb.gmt.naibeck.guiamultiturismocentroamerica.viewmodel.HomeViewModel;
+
+public class HomeActivity extends BaseActivity<ActivityHomeBinding, HomeViewModel> {
+    @Override
+    public int getLayout() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    public HomeViewModel getViewModel() {
+        return new HomeViewModel();
+    }
+
+    @Override
+    public void setViewModelToBinding() {
+        getBinding().setViewModel(getViewModel());
+    }
+}
